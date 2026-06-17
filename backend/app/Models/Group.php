d2e0ad804ae0bbdedaaa3c8cfab074d2c5d7be3c
Cedrix <cedrixdacanay27@gmail.com>
2026-06-18 00:42:47 +0800

    feat(models): establish the relationship for User and Group Model

--- backend/app/Models/Group.php
+++ backend/app/Models/Group.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
-    //
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+
+    public function bills()
+    {
+        return $this->hasMany(Bill::class);
+    }
 }
