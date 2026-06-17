6c0778974c3cda7c0aae9ef3f4f4ac351f598083
Cedrix <cedrixdacanay27@gmail.com>
2026-06-18 00:34:07 +0800

    feat(database): create initial models and migrations for database schema

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function() {
+    return response()->json(['message' => 'Api is Running!']);
+});
+
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
